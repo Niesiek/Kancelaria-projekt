568b7405739eb20f5d05656b159d0b701fe5ed23
Niesiek <kacper.proczka@o2.pl>
2024-12-10 22:28:19 +0100

    Update pagetwo.js

--- JavaScript/pagetwo.js
+++ JavaScript/pagetwo.js
@@ -1,4 +1,4 @@
-fetch('/Text/O_kancelarii.txt')
+fetch('./Text/O_kancelarii.txt')
             .then(response => {
                 if (!response.ok) {
                     throw new Error('Błąd wczytywania pliku!');
