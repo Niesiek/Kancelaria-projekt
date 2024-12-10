e1604d3e2d1b6e59767f1070dee20ca1188c9630
Kacper Proczka <kacperproczka@laptop-Kacper.local>
2024-12-10 19:28:54 +0100

    First

---
+++ JavaScript/pagethree.js
@@ -0,0 +1,17 @@
+fetch('/Text/czymsiezajmuje.txt')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Błąd wczytywania pliku!');
+        }
+        return response.text();
+    })
+    .then(data => {
+        const formattedData = data
+            .replace(/([:,])/g, '$1<br>') 
+            .replace(/([.!?])\s*(?=[A-Z])/g, '$1<br>');
+
+        document.getElementById('tekst').innerHTML = `<p>${formattedData}</p>`;
+    })
+    .catch(error => {
+        console.error('Wystąpił problem:', error);
+    });
---
+++ JavaScript/pagetwo.js
@@ -0,0 +1,15 @@
+fetch('/Text/O_kancelarii.txt')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Błąd wczytywania pliku!');
+                }
+                return response.text();
+         })
+    .then(data => {
+        document.getElementById('tekst').textContent = data;
+    })
+    .catch(error => {
+        console.error('Wystąpił problem:', error);
+    });
+
+
